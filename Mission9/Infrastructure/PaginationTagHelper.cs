2229e6604699c8b41ca59f8c9d4b124520af04d7
Parker Warner <plouwarner@gmail.com>
2023-02-28 22:11:19 -0700

    Pagination and display changes

--- Mission9/Infrastructure/PaginationTagHelper.cs
+++ Mission9/Infrastructure/PaginationTagHelper.cs
@@ -31,6 +31,12 @@ namespace Mission9.Infrastructure
         public PageInfo PageModel { get; set; } //Translated from the html tag to this name
         public string PageAction { get; set; }
 
+        //Styling variables
+        public bool PageClassesEnabled { get; set; } = false;
+        public string PageClass { get; set; }
+        public string PageClassNormal { get; set; }
+        public string PageClassSelected { get; set; }
+
         public override void Process(TagHelperContext thc, TagHelperOutput tho)
         {
             IUrlHelper uh = uhf.GetUrlHelper(vc);
@@ -42,6 +48,12 @@ namespace Mission9.Infrastructure
                 TagBuilder tb = new TagBuilder("a");
 
                 tb.Attributes["href"] = uh.Action(PageAction, new { pageNum = i });
+                if (PageClassesEnabled)
+                {
+                    tb.AddCssClass(PageClass);
+                    tb.AddCssClass(i == PageModel.CurrentPage
+                        ? PageClassSelected : PageClassNormal);
+                }
                 tb.InnerHtml.Append(i.ToString());
 
                 final.InnerHtml.AppendHtml(tb);
