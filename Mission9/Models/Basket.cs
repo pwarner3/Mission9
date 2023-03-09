9c4da9958036ab35a86c6bba802e74cae44b0fce
Parker Warner <plouwarner@gmail.com>
2023-03-08 17:03:39 -0700

    Basket Model

---
+++ Mission9/Models/Basket.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission9.Models
+{
+    public class Basket
+    {
+        public List<BasketLineItem> Items { get; set; } = new List<BasketLineItem>();
+
+        public void AddItem(Book book, int qty)
+        {
+            BasketLineItem line = Items
+                .Where(x => x.Book.BookId == book.BookId)
+                .FirstOrDefault();
+
+            if (line == null) // Checks if there is an entry for line item and will either create a new one or just add the quantity
+            {
+                Items.Add(new BasketLineItem
+                {
+                    Book = book,
+                    Quantity = qty
+                });
+            }
+            else
+            {
+                line.Quantity += qty;
+            }
+        }
+
+        public double CalculateTotal()
+        {
+            double sum = Items.Sum(x => x.Quantity * 25);
+
+            return sum;
+        }
+    }
+
+    public class BasketLineItem
+    {
+        public int LineId { get; set; }
+        public Book Book { get; set; }
+        public int Quantity { get; set; }
+    }
+}
