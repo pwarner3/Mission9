849e8f89b44831cfa64763f5b501551809d1c4f3
Parker Warner <plouwarner@gmail.com>
2023-03-15 13:51:57 -0600

    Add Cart Icon and Summary

---
+++ Mission9/Components/CartSummaryViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Mission9.Models;
+
+namespace Mission9.Components
+{
+    public class CartSummaryViewComponent : ViewComponent
+    {
+        private Basket basket;
+        public CartSummaryViewComponent(Basket cartService)
+        {
+            basket = cartService;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(basket);
+        }
+    }
+}
